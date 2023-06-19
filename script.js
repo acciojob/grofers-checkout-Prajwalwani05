6f01f430a79b5333b50e8a7d35d604e1345c3a8a
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-06-19 19:17:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,17 +4,43 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-	let i = document.getElementsByClassName("price")[0];
-	let list = i.value;
-	let total = document.createElement("div");
-	total.innerText = list;
-	let sum=0;
-	for(let x=0;x<total.length;x++){
-		sum += total[x];
-	}
-	let totalSum = document.createElement('p');
-	totalSum.textContent = sum;
-  document.append(totalSum);
+	const table = document.querySelector('#grocery-table');
+
+  // Get all the price elements
+  const priceElements = table.querySelectorAll('.prices');
+
+  let totalPrice = 0;
+
+  // Loop through each price element and sum the prices
+  priceElements.forEach((priceElement) => {
+    const price = parseFloat(priceElement.textContent.replace('$', ''));
+    totalPrice += price;
+  });
+
+  // Check if the total price row already exists
+  const totalRow = table.querySelector('#total-row');
+  if (totalRow) {
+    // Update the total price cell
+    const totalCell = totalRow.querySelector('.total-price');
+    totalCell.textContent = 'Total Price: $' + totalPrice.toFixed(2);
+  } else {
+    // Create a new row element
+    const newRow = document.createElement('tr');
+    newRow.id = 'total-row';
+
+    // Create a cell for the total price
+    const totalCell = document.createElement('td');
+    totalCell.setAttribute('colspan', '2');
+    totalCell.classList.add('total-price');
+    totalCell.textContent = 'Total Price: $' + totalPrice.toFixed(2);
+
+    // Append the cell to the row
+    newRow.appendChild(totalCell);
+
+    // Append the row to the table
+    table.appendChild(newRow);
+  }
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
