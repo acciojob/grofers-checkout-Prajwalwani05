1e6aed32f9ac7d764d6f89e0d0de01e6cb843a69
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-06-19 19:15:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let i = document.getElementsByClassName("price")[0];
+	let list = i.value;
+	let total = document.createElement("div");
+	total.innerText = list;
+	let sum=0;
+	for(let x=0;x<total.length;x++){
+		sum += total[x];
+	}
+	let totalSum = document.createElement('p');
+	totalSum.textContent = sum;
+  document.append(totalSum);
 };
 
 getSumBtn.addEventListener("click", getSum);
